2aae4ea88200ed54135649420afe235a6f16706a
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-04-02 19:40:00 +0530

    added 3 endpoints

--- signalling-server/src/index.ts
+++ signalling-server/src/index.ts
@@ -1,16 +1,33 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 
-let senderSocket: null | WebSocket = null;
-let receiverSocket: null | WebSocket = null;
+let senderSocket: null| WebSocket = null;
+let receiverSocket: any= null;
+
+
 
 wss.on('connection',function connection(ws){
     ws.on('error',console.error)
     ws.on('message', function message(data: any) {
         const message = JSON.parse(data);
         console.log(message)
+        //we have to support the following messages to establisha a webrtc connection
+        //identify-as-sender
+        //identify-as-receiver
+
+        if(message.type==="identify-as-sender"){
+            senderSocket = ws;
+        }else if(message.type==="identify-as-receiver"){
+            receiverSocket = ws;
+        }else if (message.type === 'create-offer'){
+            receiverSocket.send(JSON.stringify({type:'offer', offer:message.offer}))
+        }
         
+        //create Offer
+        //create Answer
+        //add ice candidate
+
       });
 })
