6548937a6c258db1561cd011c93c5711548b1b15
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-04-02 20:41:56 +0530

    initiated ws connections from both sender and receiver clients

--- frontend/src/components/Receiver.tsx
+++ frontend/src/components/Receiver.tsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react"
+
 export function Receiver(){
-    return <div> 
-      receiver
-    </div>
-  }
+
+
+    useEffect(()=>{
+        const socket = new WebSocket('ws://localhost:8000')
+
+         socket.onopen=()=>{
+          socket.send(JSON.stringify({type:"receiver"}))
+         }
+    },[])
+
+
+
+
+
+  return <div> 
+
+
+
+
+
+
+
+
+    receiver
+  </div>
+}
--- frontend/src/components/Sender.tsx
+++ frontend/src/components/Sender.tsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react"
+
 export function Sender(){
+
+
+    useEffect(()=>{
+        const socket = new WebSocket('ws://localhost:8000')
+
+         socket.onopen=()=>{
+          socket.send(JSON.stringify({type:"sender"}))
+         }
+    },[])
+
+
+
+
+
   return <div> 
+
+
+
+
+
+
+
+
     sender
   </div>
 }
